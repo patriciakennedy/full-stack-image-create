6429025a8929f6c353ca251283053a1d8e91fe6f
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-01-10 14:27:56 -0600

    Set up basic Express server with a root route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-//server.js is the entry point for our application
+// import express and create an express app
+// const express = require('express'); traditional way of importing
+
+// a modern syntax - es modules
+// in other to use it  add it in the package.json file like so
+// "type": "module",
+import express from "express";
+
+// calls the express function to create an app instance
+const app = express();
+
+// lets create a route
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.listen(5000, () => {
+  console.log('server started at http://localhost:5000');
+});
+
+
