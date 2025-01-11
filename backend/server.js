777ccd4ad894b1d22bf9da4de8b2da6f49dbe6bb
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-01-10 18:29:34 -0600

    Add MongoDB/Atlas connection to the application

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,19 @@
 // in other to use it  add it in the package.json file like so
 // "type": "module",
 import express from "express";
+import dotenv from "dotenv"; // mongoDB connection string lives in the .env file
 
 // calls the express function to create an app instance
 const app = express();
 
-// lets create a route
-app.get("/", (req, res) => {
+// lets create a route ROTE
+app.get("/products", (req, res) => {
   res.send("Server is ready");
+});
+
+// Create a ROUTE for all products in our database
+app.get('/products', (req, res) => {
+  res.send('Server is ready');
 });
 
 app.listen(5000, () => {
