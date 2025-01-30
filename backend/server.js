6f5fb2993998ff4635e74c5d141b6c55be5f02f4
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-01-30 11:46:02 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@
 // Import the function to connect to MongoDB
 import express from "express";
 import dotenv from "dotenv"; 
-import { connectDB } from './config/db.js';
+import { connectDB } from "./config/db.js";
 
 // Load environment variables (e.g., database connection string)
 dotenv.config();
@@ -12,19 +12,47 @@ dotenv.config();
 // Create an Express application
 const app = express();
 
+//middleware: allows us to accept JSON data in the req.body
+//middleware: function that runs before you send response back to the client
+app.use(express.json());
+
 // Route to check if the server is working
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
 
 // Placeholder route to handle product-related requests (no response yet)
-app.get("/products", (req, res) => {});
+app.post("/api/products", async (req, res) => {
+  const product = req.body; //user will send this data
 
+  //check for the requirements
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please provide all fields' });
+  }
+
+  //if all fields have data - check PASS - create a new product
+  const newProduct = new Product(product);
+
+  //we setup a try-catch => is a way to handle errors in JavaScript.
+  //try: Runs the code that might cause an error.
+  //catch: Runs if an error happens, so your app doesn't crash.
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error('Error in create product:', error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+});
+
+//NEXT==> TO TESET IT OUT WITHOUT A FRONT END APPLICATION WE WILL USE 'POSTMAN' DESKTOP APP
 
 // Start the server and connect to MongoDB
 app.listen(5000, () => {
   connectDB(); // Connect to the database
-  console.log('server started at http://localhost:5000');
+  console.log("server started at http://localhost:5000");
 });
 
 
--- model/product.model.js
+++ model/product.model.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-//first create a schema
+//----------------------first create a schema ------------------------------------
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -14,6 +14,14 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-}, {
+}, 
+{
   timestamps: true //createAt, updateAt
 });
+
+//------------------------create product model------------------------------------
+//code tells mongoDB to create a collection call 'Product' & use 'productSchema'
+const Product = mongoose.model('Product', productSchema);
+//while 'Product' uppercase P? //mongo will convert it to 'products' it just want you to put the uppercase 'P' and singular. It will handle the rest
+export default Product;
+
